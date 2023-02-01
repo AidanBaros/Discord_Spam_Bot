83065c2d2995a1f005cad0e9964a051e373a57d3
KINGOODAN <aidanlhbaros@gmail.com>
2023-02-01 13:10:02 -0700

    Fun things

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+f = open("spam.txt", "r")
+
+for word in f:
+    pyautogui.typewrite(word)
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
